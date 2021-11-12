0f9c3a53e9893fb9846ef084c95f675957b07b60
Jaka061 <jrkny700@gmail.com>
2021-11-12 12:51:00 +0600

    hw8 svetofor

--- app/src/main/java/com/android_ls1/MainActivity.kt
+++ app/src/main/java/com/android_ls1/MainActivity.kt
@@ -1,44 +1,58 @@
-package com.android_ls1
+package com.android_ls1 // hw-8 Светофор .
 
-import android.annotation.SuppressLint
+import android.graphics.Color.parseColor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_sec.*
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("WrongViewCast")
+    private lateinit var  txt1 : AppCompatTextView
+    private lateinit var svet : ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.e("Tag","Ex:create")  // LogCat Error ( выводит ошибки )
+        setContentView(R.layout.activity_sec)
+
+        txt1 = findViewById<AppCompatTextView>(R.id.hello1)
+        svet = findViewById(R.id.svetofor)
+        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
+        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
+        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
+
+        btn1.setOnClickListener {
+            onClick(it)
+        }
+        btn2.setOnClickListener {
+            onClick(it)
+        }
+        btn3.setOnClickListener {
+            onClick(it)
+        }
+    }
+    private fun onClick(v: View){
+        when(v.id){
+            R.id.btn1 -> {
+                txt1.text = "Красный Стой"
+                svet.setBackgroundColor(parseColor("red"))   // есть разница в использовании svet и svetofor, Они одинаково работают ?
+                btn1.setBackgroundColor(parseColor("white"))
+            }
+            R.id.btn2 -> {
+                txt1.text = "Желтый Жди "
+                svetofor.setBackgroundColor(parseColor("yellow"))
+                btn2.setBackgroundColor(parseColor("white"))
+            }
+            R.id.btn3 -> {
+                txt1.text = "Зеленый Иди "
+                svetofor.setBackgroundColor(parseColor("green"))
+                btn3.setBackgroundColor(parseColor("white"))
+            }
+        }
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.e("  Tag","Ex:start")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.e("Tag","Ex:resume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.e("Tag","Ex:pause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
 }
