f5b7e43125f8e6cc207ea1a3983c886cb6bfb721
Jaka061 <jrkny700@gmail.com>
2021-11-17 17:40:51 +0600

    hw 9

--- app/src/main/java/com/android_ls1/MainActivity.kt
+++ app/src/main/java/com/android_ls1/MainActivity.kt
@@ -1,58 +1,37 @@
-package com.android_ls1 // hw-8 Светофор .
+package com.android_ls1
 
-import android.graphics.Color.parseColor
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import kotlinx.android.synthetic.main.activity_sec.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var  txt1 : AppCompatTextView
-    private lateinit var svet : ConstraintLayout
-
+    private lateinit var btn1 : AppCompatButton
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sec)
-
-        txt1 = findViewById<AppCompatTextView>(R.id.hello1)
-        svet = findViewById(R.id.svetofor)
-        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
-        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
-        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
+        setContentView(R.layout.activity_main)
+        txt1 = findViewById<AppCompatTextView>(R.id.txt)
+        btn1 = findViewById<AppCompatButton>(R.id.button)
 
         btn1.setOnClickListener {
-            onClick(it)
-        }
-        btn2.setOnClickListener {
-            onClick(it)
-        }
-        btn3.setOnClickListener {
-            onClick(it)
+            val countTxt = txt1.text.toString()
+            count = Integer.parseInt(countTxt)
+            count++
+            txt1.text = count.toString()
         }
     }
-    private fun onClick(v: View){
-        when(v.id){
-            R.id.btn1 -> {
-                txt1.text = "Красный Стой"
-                svet.setBackgroundColor(parseColor("red"))   //есть разница в использовании svet и svetofor, Они одинаково работают ?
-                btn1.setBackgroundColor(parseColor("white"))
-            }
-            R.id.btn2 -> {
-                txt1.text = "Желтый Жди "
-                svetofor.setBackgroundColor(parseColor("yellow"))
-                btn2.setBackgroundColor(parseColor("white"))
-            }
-            R.id.btn3 -> {
-                txt1.text = "Зеленый Иди "
-                svetofor.setBackgroundColor(parseColor("green"))
-                btn3.setBackgroundColor(parseColor("white"))
-            }
-        }
 
+    override fun onSaveInstanceState (outState : Bundle){
+        super.onSaveInstanceState(outState)
+        outState.putInt("count",count)
     }
-
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt("count")
+    }
 }
