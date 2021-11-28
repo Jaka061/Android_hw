387e501e802342083d65386fe6927e00b11cef79
Jaka061 <jrkny700@gmail.com>
2021-11-28 14:29:57 +0600

    ls 23 fragment

--- app/src/main/java/com/android/MainActivity.kt
+++ app/src/main/java/com/android/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SimpleAdapter
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,21 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recycle = findViewById<RecyclerView>(R.id.recycler)
-        val layMan = LinearLayoutManager(this)
-        val adapter = SimpleAdapter{
-            Toast.makeText(this,"Item -$it",Toast.LENGTH_SHORT).show()
-        }
-
-        recycle.layoutManager = layMan
-        recycle.adapter =adapter
-        recycle.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
 
 
-        val list = mutableListOf<String>()
-        for (i in 0..20) {
-            list.add("ITEM -$i")
+//        val recycle = findViewById<RecyclerView>(R.id.recycler)
+//        val layMan = LinearLayoutManager(this)
+//        val adapter = SimpleAdapter{
+//            Toast.makeText(this,"Item -$it",Toast.LENGTH_SHORT).show()
+//        }
+//
+//        recycle.layoutManager = layMan
+//        recycle.adapter =adapter
+//        recycle.addItemDecoration(DividerItemDecoration(this, RecyclerView.VERTICAL))
+//
+//
+//        val list = mutableListOf<String>()
+//        for (i in 0..20) {
+//            list.add("ITEM -$i")
+//        }
+//        adapter.setData(list)
         }
-        adapter.setData(list)
-        }
+
 }
