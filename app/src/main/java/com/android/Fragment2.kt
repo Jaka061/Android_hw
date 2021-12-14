cf7c8ff9d8082f16db5d2ac6d5054e3cc53d8b3c
Kudaybergen <tuleubekov.kas@gmail.com>
2021-12-14 08:24:37 +0600

    comment: hw-18

--- app/src/main/java/com/android/Fragment2.kt
+++ app/src/main/java/com/android/Fragment2.kt
@@ -6,7 +6,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 
 // hw-18: Нету стрелки назад в тулбаре
-// hw-18: Названия классам следует давать осмысленные -> AboutFragment, MainFragmetn и тд. На классных уроках так называли для примера
+// hw-18: Названия классам следует давать осмысленные -> AboutFragment, MainFragmetn, AuthFragment и тд. На классных уроках так называли для примера
 
 class Fragment2 : Fragment(R.layout.fragment_about) {
 
