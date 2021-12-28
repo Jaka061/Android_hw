a528811ac43730268e1cab3e05b16f580469ffb7
Jaka061 <jrkny700@gmail.com>
2021-12-28 19:28:43 +0600

    hw 33

--- app/src/main/java/com/android/MainActivity.kt
+++ app/src/main/java/com/android/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.android
 
 import android.os.Bundle
-import android.util.Log
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.android.databinding.ActivityMain2Binding
-import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
@@ -14,133 +14,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initClickListener()
-
         binding.apply {
+            edit.addTextChangedListener(object: TextWatcher {
+                override fun afterTextChanged(s: Editable?) {}
 
-            plus.setOnClickListener {
-                plus()
-            }
-            minus.setOnClickListener {
-                minus()
-            }
-            multiply.setOnClickListener {
-                multiply()
-            }
-            divider.setOnClickListener {
-                divider()
-            }
+                override fun beforeTextChanged(s: CharSequence?,start: Int,count: Int, after: Int) {}
 
-            delete.setOnClickListener {
-                delete()
-            }
-            result.setOnClickListener {
-                result()
-            }
-        }
-    }
-    private fun txtCalc (string : String ) {
-        binding.txtInput.append(string)
-        if(binding.txtResult.text.isNotEmpty()){
-            binding.txtResult.text=""
-        }
-    }
-
-    private fun initClickListener(){
-        binding.apply {
-            zero.setOnClickListener { txtCalc("0") }
-            one.setOnClickListener { txtCalc("1") }
-            two.setOnClickListener { txtCalc("2") }
-            three.setOnClickListener { txtCalc("3") }
-            four.setOnClickListener { txtCalc("4") }
-            five.setOnClickListener { txtCalc("5") }
-            six.setOnClickListener { txtCalc("6") }
-            seven.setOnClickListener { txtCalc("7") }
-            eight.setOnClickListener { txtCalc("8") }
-            nine.setOnClickListener { txtCalc("9") }
-            openH.setOnClickListener { txtCalc("(") }
-            closeH.setOnClickListener { txtCalc(")") }
-            point.setOnClickListener { txtCalc(".") }
-            clean.setOnClickListener {
-                txtInput.text = " "
-                txtResult.text = ""
-            }
-        }
-    }
-
-    fun minus(){
-        binding.apply {
-            val txt = txtInput.text.toString()
-            if (txt.isNotEmpty()) {
-                txtInput.append("-").toString()
-            } else {
-                txtInput.append("0-").toString()
-            }
-        }
-    }
-
-    fun plus(){
-        binding.apply {
-            val txt = txtInput.text.toString()
-            if (txt.isNotEmpty()) {
-                txtInput.append("+").toString()
-            } else {
-                txtInput.append("0+").toString()
-            }
-        }
-    }
-
-    private fun multiply(){
-        binding.apply {
-            val txt = txtInput.text.toString()
-            if (txt.isNotEmpty()) {
-                txtInput.append("*").toString()
-            } else {
-                txtInput.append("0*").toString()
-            }
-        }
-    }
-
-    private fun divider(){
-        binding.apply {
-            val txt = txtInput.text.toString()
-            if (txt.isNotEmpty()) {
-                txtInput.append("/").toString()
-            } else {
-                txtInput.append("0/").toString()
-            }
-        }
-    }
-
-    private fun delete(){
-        binding.apply {
-            val txt = txtInput.text.toString()
-            if (txt.isNotEmpty()) {
-                txtInput.text = txt.substring(0, txt.length - 1)
-            }
-            txtResult.text = ""
-        }
-    }
-
-    private fun result(){
-        binding.apply {
-            try {
-                val res = ExpressionBuilder(txtInput.text.toString()).build()
-                val res1 = res.evaluate()
-
-                val long = res1.toLong()
-                if (res1 == long.toDouble()) {
-                    txtResult.text = long.toString()
-                } else {
-                    txtResult.text = res1.toString()
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                   btn.setOnClickListener {
+                       val words = edit.text.toString().trim()
+                       val count = Regex("\\w+").findAll(words.trim()).count()    // можно кнопку в целом не использовать
+                       txt.text = "$count"
+                   }
                 }
-            } catch (e: ArithmeticException) {
-                Log.e("Ошибка :", " $e")
-                txtResult.text = "На 0 делить нельзя !"
-            } catch (e: Exception) {
-                Log.e("Ошибка :", " $e")
-                txtResult.text = "Ошибка !"
-            }
+            })
         }
     }
+
 }
