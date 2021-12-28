ad7bec7e1e541d8c14e586dab5ef4a47e52a2ba9
Jaka061 <jrkny700@gmail.com>
2021-12-28 21:55:23 +0600

    hw-34

--- app/src/main/java/com/android/MainActivity.kt
+++ app/src/main/java/com/android/MainActivity.kt
@@ -20,12 +20,12 @@ class MainActivity : AppCompatActivity() {
 
                 override fun beforeTextChanged(s: CharSequence?,start: Int,count: Int, after: Int) {}
 
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                   btn.setOnClickListener {
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {        //[oO] - o & O
+
                        val words = edit.text.toString().trim()
-                       val count = Regex("\\w+").findAll(words.trim()).count()    // можно кнопку в целом не использовать
+                       val count = words.split("\\b[a-np-zA-NP-Z]*[oO][a-np-zA-NP-Z]*\\b".toRegex()).size - 1
                        txt.text = "$count"
-                   }
+
                 }
             })
         }
