81bc71152d15d0f4f286aeccd42989ce6be34bc4
Jaka061 <jrkny700@gmail.com>
2021-12-29 17:43:47 +0600

    hw 31

--- app/src/main/java/com/android/MainActivity.kt
+++ app/src/main/java/com/android/MainActivity.kt
@@ -2,13 +2,25 @@ package com.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(),OnClick {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val menu = findViewById<NavigationView>(R.id.menu)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
         supportFragmentManager.beginTransaction()
             .add(R.id.frag_cont,AutorizationFragment()).commit()
     }
