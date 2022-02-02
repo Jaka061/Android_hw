f29cedcbb44d5e4cb6f3a2af0f29922fe6c1e9cf
Zharkynai <jrkny700@gmail.com>
2022-02-02 23:44:03 +0600

    hw-38

--- app/src/main/java/com/android/MainActivity.kt
+++ app/src/main/java/com/android/MainActivity.kt
@@ -1,34 +1,42 @@
 package com.android
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.android.databinding.ActivityMain2Binding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMain2Binding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        var countMax = 0
+        var numA: Int
+        var wrdMax = ""
+
 
         binding.apply {
-            edit.addTextChangedListener(object: TextWatcher {
-                override fun afterTextChanged(s: Editable?) {}
+              btn.setOnClickListener {
+                  val listA = edit.text.toString().trim().split("\\s+".toRegex())
 
-                override fun beforeTextChanged(s: CharSequence?,start: Int,count: Int, after: Int) {}
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                   btn.setOnClickListener {
-                       val words = edit.text.toString().trim()
-                       val count = Regex("ая\\b").findAll(words.trim()).count()
-                       txt.text = "$count"
-                   }
-                }
-            })
+                  listA.forEach {
+                      if(it.contains("a")){
+                          numA = it.count { wrd -> wrd == 'a' }
+                          if(countMax < numA){
+                              countMax = numA
+                              wrdMax = it
+                          }
+                      }
+                  }
+                  txt.text = wrdMax + "- $countMax"
+              }
         }
-    }
 
+    }
 }
+
+// работает , но если стереть некоторые записи результат не меняется .
+// Учитывает  только новые записи не смогла этот момент доработать (
